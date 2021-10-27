ac09b702f333bddbdbb33ec8b6b7ecefd059caa7
ixboy <ismaelixboy@gmail.com>
2021-10-27 09:22:28 +0100

    create calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import 'Calculator.css';
+
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+}

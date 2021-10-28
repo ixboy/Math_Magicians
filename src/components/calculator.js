cb164a27fd4f8643b4a47eb6ca27b03f967533a7
ixboy <ismaelixboy@gmail.com>
2021-10-28 02:49:21 +0100

    remove unused code

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,9 +2,7 @@ import './calculator.css';
 
 import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     this.createDigits = () => {

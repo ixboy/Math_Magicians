2e9f63d5b39a692f06f0a7410d8bf99166b47dec
ixboy <ismaelixboy@gmail.com>
2021-10-29 22:56:14 +0100

    refactor Ap to use functional components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
 
-import React, { Component } from 'react';
-
 import Calculator from './components/calculator';
 
-/* eslint-disable react/prefer-stateless-function */
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (<div className="App"><Calculator /></div>);
 
 export default App;

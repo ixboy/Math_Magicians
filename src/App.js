d40bb2f69f83e61919f8f50a623767d90fcb56d9
ixboy <ismaelixboy@gmail.com>
2021-11-03 20:27:41 +0100

    import all pages and add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
 
-import Calculator from './components/calculator';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
 
-const App = () => (<div className="App"><Calculator /></div>);
+import Calculator from './components/Calculator';
+import Home from './components/pages/Home';
+import Navbar from './components/pages/Navbar';
+import Quote from './components/pages/Quote';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home}>
+          <Home />
+        </Route>
+        <Route path="/calculator" component={Calculator}>
+          <Calculator />
+        </Route>
+        <Route path="/quote" component={Quote}>
+          <Quote />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;

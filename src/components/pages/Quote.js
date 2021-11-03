6f51b659c97070a4b3304c6656d155a2c565cef1
ixboy <ismaelixboy@gmail.com>
2021-11-03 20:26:46 +0100

    add QuotesPage component

---
+++ src/components/pages/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quotesDiv">
+    <div>
+      <h2>
+        Mathematics is not about numbers, equations,
+        computations, or algorithms: it is about understanding. - William Paul Thurston
+      </h2>
+    </div>
+  </div>
+);
+
+export default Quote;

a93cb107e0155a66e156982097a1b4af5f8cd145
ixboy <ismaelixboy@gmail.com>
2021-11-16 03:53:22 +0100

    add quote and different screenshot

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ const App = () => (
           <Home />
         </Route>
         <Route path="/calculator" component={Calculator}>
+          <div className="randomDiv">
+            <h3> Mathematics is a hard thing to love, let&apos;s do some Maths.</h3>
+          </div>
           <Calculator />
         </Route>
         <Route path="/quote" component={Quote}>
